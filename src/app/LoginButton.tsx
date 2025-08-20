0e83971ec6ff01d55d0a50532a67f5a764e60a50
Astrak <coffre.a@gmail.com>
2025-08-20 22:06:46 +0200

    Reference easiest implementation

--- src/app/LoginButton.tsx
+++ src/app/LoginButton.tsx
@@ -2,21 +2,13 @@
 
 import Button from "@/components/Button/Button";
 import { signOut, useSession } from "next-auth/react";
-import Link from "next/link";
 import { useCallback } from "react";
 
 export default function LoginButton() {
   const { data: session } = useSession();
   const isAuthenticated = !!session;
   const click = useCallback(async () => {
-    if (isAuthenticated) {
-      await signOut();
-      console.log("signed out");
-    }
-  }, [isAuthenticated]);
-  return (
-    <Link href="/login">
-      <Button onClick={click}>{isAuthenticated ? "Log out" : "Log in"}</Button>
-    </Link>
-  );
+    await signOut();
+  }, []);
+  return isAuthenticated && <Button onClick={click}>Log out</Button>;
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,16 @@
-export default function Home() {
-  return <p>{"You're logged out"}</p>;
+"use client";
+
+import Button from "@/components/Button/Button";
+import { signIn } from "next-auth/react";
+import { useCallback } from "react";
+
+export default function Login() {
+  const sign = useCallback(() => {
+    signIn("google");
+  }, []);
+  return (
+    <div onClick={sign} className=" cursor-pointer">
+      <Button onClick={sign}>Log in with Google</Button>
+    </div>
+  );
 }
