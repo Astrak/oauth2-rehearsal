76843bc2ea4738649b88aa17c91ae0038767c855
Astrak <coffre.a@gmail.com>
2025-08-20 21:16:40 +0200

    Add basic NextAuth session management finally

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -1,40 +1,13 @@
 import NextAuth from "next-auth";
-import { JWT } from "next-auth/jwt";
 import GoogleProvider from "next-auth/providers/google";
 
-const auth = () =>
-  NextAuth({
-    providers: [
-      GoogleProvider({
-        clientId: process.env.OAUTH_GOOGLE_CLIENT_ID ?? "",
-        clientSecret: process.env.OAUTH_GOOGLE_CLIENT_SECRET ?? "",
-      }),
-    ],
-    session: {
-      strategy: "jwt",
-    },
-    callbacks: {
-      async jwt({ token, user, account, profile, trigger, isNewUser }) {
-        if (user) {
-          token.name = user.name;
-          token.email = user.email;
-          token.picture = user.image;
-          token.sub = user.id;
-        }
-        return token;
-      },
-      async session({ session, token }) {
-        if (token && session.user) {
-          session.user.name = token.name || session.user.name;
-          session.user.email = token.email || session.user.email;
-          session.user.image = token.picture || session.user.image;
-        }
-        return session;
-      },
-      async signIn({ user, account, email, credentials }) {
-        return true;
-      },
-    },
-  });
+const auth = NextAuth({
+  providers: [
+    GoogleProvider({
+      clientId: process.env.OAUTH_GOOGLE_CLIENT_ID ?? "",
+      clientSecret: process.env.OAUTH_GOOGLE_CLIENT_SECRET ?? "",
+    }),
+  ],
+});
 
 export { auth as GET, auth as POST };
